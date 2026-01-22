1e1d697150ca69e909e65ff9a3c8a72530b136ab
Tyron <tyronjf44@gmail.com>
2026-01-22 08:18:58 -0500

    Adding files from Mac

---
+++ LabEX1_2031/LabEX1_2031 macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  LabEX1_2031 macOS
+//
+//  Created by Tyron Fajardo on 2026-01-22.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
