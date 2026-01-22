7e7c49d1bf860cdadbfa205394f8ec6892f636e1
Tyron Fajardo <tyronjf44@gmail.com>
2026-01-22 08:36:02 -0500

    Changing the text to my name

--- LabEX1_2031/LabEX1_2031 Shared/GameScene.swift
+++ LabEX1_2031/LabEX1_2031 Shared/GameScene.swift
@@ -31,6 +31,7 @@ class GameScene: SKScene {
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
+            label.text = "Tyron Fajardo"
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
